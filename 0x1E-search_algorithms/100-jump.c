36abc5f7c7db880d2bd36f189e3bac9b8bc68e56
Edet Isong <eddieisong1@gmail.com>
2023-12-15 14:56:47 -0800

    My C Project on 0x1E. C - Search Algorithms by Edet Isong

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,7 +2,7 @@
 
 /**
 * linear-search - looks for a value in an array
-* 		  of integers using linear search
+*		of integers using linear search
 * @array: A pointer to the first element of the array to search
 * @size: The number of elements in the array
 * @value: The value to search for
@@ -14,7 +14,7 @@
 int jump_search(int *array, size_t size, int value)
 {
 	size_t i, jump, step;
-	
+
 	/* Check if the array is NULL or empty */
 	if (array == NULL || size == 0)
 		return (-1);
@@ -26,7 +26,7 @@ int jump_search(int *array, size_t size, int value)
 	for (i = jump = 0; jump < size && array[jump] < value;)
 	{
 		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
-		
+
 		/* Store the previous jump position */
 		i = jump;
 
@@ -44,7 +44,7 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
 	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-	
+
 	/* Check if the value is found and return the corresponding index */
 	return (array[i] == value ? (int)i :  -1);
 }
